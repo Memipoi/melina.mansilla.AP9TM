a6343871ab1e63d3d1a4549491becb755460f353
DESKTOP-A8SNUHP\melin <melinpucca@live.com>
2023-08-04 00:18:40 -0300

    My repo and my first client created.

---
+++ src/main/java/com/mindhub/homebanking/models/Client.java
@@ -0,0 +1,61 @@
+package com.mindhub.homebanking.models;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+        private long id;
+        private String dni;
+        private String name;
+        private String lastName;
+        private String email;
+        public Client(){
+
+        }
+    public Client(String dni, String name, String lastName, String email) {
+        this.dni = dni;
+        this.name = name;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
